a0e0ff961a8453a6322c81fe696a3f3417858e49
tchun <tchun@carparts.com>
2023-03-19 04:04:52 -0700

    all files

---
+++ dev_d_one/frontend/test_next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_DJANGO_API_URL: process.env.NEXT_PUBLIC_DJANGO_API_URL,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/apiv01/:path*',
+        destination: `${process.env.NEXT_PUBLIC_DJANGO_API_URL}/apiv01/:path*`,
+      },
+    ];
+  },
+};
